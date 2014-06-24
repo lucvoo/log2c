5b885b36a561d5527fa9280a01b9fbcd32c1ede2
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-06-24 18:47:39 +0200

    [CLEANUP] Simplify the generation of the foreign predicate prototypes

--- src/pl-ext.h
+++ src/pl-ext.h
@@ -13,25 +13,34 @@
 #define PL_PROTO_D(A)	PL_PROTO_D_##A
 #define PL_PROTO_N(A)	PL_PROTO_N_##A
 
-#define PL_PROTO_D_0	void);
-#define PL_PROTO_D_1	union cell *);
-#define PL_PROTO_D_2	union cell *, union cell *);
-#define PL_PROTO_D_3	union cell *, union cell *, union cell *);
-#define PL_PROTO_D_4	union cell *, union cell *, union cell *, union cell *);
-#define PL_PROTO_D_5	union cell *, union cell *, union cell *, union cell *, union cell *);
-#define PL_PROTO_D_6	union cell *, union cell *, union cell *, union cell *, union cell *, union cell *);
-#define PL_PROTO_D_7	union cell *, union cell *, union cell *, union cell *, union cell *, union cell *, union cell *);
-#define PL_PROTO_D_8	union cell *, union cell *, union cell *, union cell *, union cell *, union cell *, union cell *, union cell *);
+#define PL_PROTO_1	union cell *
+#define PL_PROTO_2	PL_PROTO_1, union cell *
+#define PL_PROTO_3	PL_PROTO_2, union cell *
+#define PL_PROTO_4	PL_PROTO_3, union cell *
+#define PL_PROTO_5	PL_PROTO_4, union cell *
+#define PL_PROTO_6	PL_PROTO_5, union cell *
+#define PL_PROTO_7	PL_PROTO_6, union cell *
+#define PL_PROTO_8	PL_PROTO_7, union cell *
 
-#define PL_PROTO_N_0	enum control *);
-#define PL_PROTO_N_1	union cell *, enum control *);
-#define PL_PROTO_N_2	union cell *, union cell *, enum control *);
-#define PL_PROTO_N_3	union cell *, union cell *, union cell *, enum control *);
-#define PL_PROTO_N_4	union cell *, union cell *, union cell *, union cell *, enum control *);
-#define PL_PROTO_N_5	union cell *, union cell *, union cell *, union cell *, union cell *, enum control *);
+#define PL_PROTO_D_0	void
+#define PL_PROTO_D_1	PL_PROTO_1
+#define PL_PROTO_D_2	PL_PROTO_2
+#define PL_PROTO_D_3	PL_PROTO_3
+#define PL_PROTO_D_4	PL_PROTO_4
+#define PL_PROTO_D_5	PL_PROTO_5
+#define PL_PROTO_D_6	PL_PROTO_6
+#define PL_PROTO_D_7	PL_PROTO_7
+#define PL_PROTO_D_8	PL_PROTO_8
 
-#define DET(P,A,F)	int F(PL_PROTO_D(A)
-#define NDET(P,A,F)	int F(PL_PROTO_N(A)
+#define PL_PROTO_N_0
+#define PL_PROTO_N_1	PL_PROTO_1,
+#define PL_PROTO_N_2	PL_PROTO_2,
+#define PL_PROTO_N_3	PL_PROTO_3,
+#define PL_PROTO_N_4	PL_PROTO_4,
+#define PL_PROTO_N_5	PL_PROTO_5,
+
+#define DET(P,A,F)	int F(PL_PROTO_D(A));
+#define NDET(P,A,F)	int F(PL_PROTO_N(A) enum control *);
 
 #else
 #define DET(F,N,C)      foreign_pred_builtin(F,N,C,det).
