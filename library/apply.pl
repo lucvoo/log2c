fda6fb7a0ff2d6f222f2215b9c9b80cb9b9fae8f
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-06-28 18:09:22 +0200

    [RUNTIME] Add foldl/4

--- library/apply.pl
+++ library/apply.pl
@@ -6,11 +6,18 @@
 /************************************************************************/
 
 :- module('$apply', [
+		foldl/4,
 		maplist/2,
 		maplist/3,
 		sublist/3
 	]).
 
+
+:- meta_predicate foldl(3, +, +, -).
+foldl(_, [], R, R).
+foldl(G, [E|Q], I, O) :-
+	call(G, E, I, T),
+	foldl(Q, G, T, O).
 
 :- meta_predicate maplist(1, ?).
 maplist(G, [A|X]) :-
