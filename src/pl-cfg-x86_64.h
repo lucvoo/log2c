8be616b1c0e14effa68988bc9701970e020d83b0
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2014-07-08 00:37:38 +0200

    [STACK] increase the stack size for x86-64

--- src/pl-cfg-x86_64.h
+++ src/pl-cfg-x86_64.h
@@ -7,10 +7,10 @@
 
 #ifdef    STACK_DEF
 //         symbol  , address   , incr , max  , name
-STACK_DEF(HEAP_STK, 0x100000000000, 1 << 28, 1 << 30, "heap")
-	STACK_DEF(SHEAP_STK, 0x101000000000, 1 << 28, 1 << 30, "sheap")
-	STACK_DEF(LOCAL_STK, 0x102000000000, 1 << 28, 1 << 30, "local")
-	STACK_DEF(TRAIL_STK, 0x103000000000, 1 << 28, 1 << 30, "trail")
+STACK_DEF(HEAP_STK, 0x100000000000, 1 << 30, 1 << 30, "heap")
+	STACK_DEF(SHEAP_STK, 0x101000000000, 1 << 30, 1 << 30, "sheap")
+	STACK_DEF(LOCAL_STK, 0x102000000000, 1 << 30, 1 << 30, "local")
+	STACK_DEF(TRAIL_STK, 0x103000000000, 1 << 30, 1 << 30, "trail")
 #endif
 #ifndef _PL_CFG_ARCH_H_
 #define _PL_CFG_ARCH_H_
